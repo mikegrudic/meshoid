1960d0c09cb512055f942eb29020b02b345fc287
Mike Grudic <mike.grudich@gmail.com>
2024-01-17 11:09:45 -0800

    flatten images

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name='meshoid',
-      version='1.42',
+      version='1.43',
       description='Package for analysis of meshless simulation data',
 #      long_description=long_description,
       url='http://github.com/mikegrudic/meshoid',
