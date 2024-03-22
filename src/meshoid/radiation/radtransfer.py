977a3cbfe85cc8f54730b7eb166df0a829048640
Mike Grudic <mgrudic@pop-os.localdomain>
2024-03-22 13:23:01 -0700

    feat: parallelized radtransfer

--- src/meshoid/radiation/radtransfer.py
+++ src/meshoid/radiation/radtransfer.py
@@ -1,6 +1,6 @@
 """Routines for radiative transfer calculations"""
 
-from numba import njit
+from numba import njit, prange, get_num_threads
 import numpy as np
 from astropy import constants
 import astropy.units as u
@@ -8,12 +8,13 @@ from ..kernel_density import *
 import os
 
 
-@njit(fastmath=True, error_model="numpy")
+@njit(fastmath=True, error_model="numpy", parallel=True)
 def radtransfer(j, m, kappa, x, h, gridres, L, center=0, i0=0):
     """Simple radiative transfer solver
 
     Solves the radiative transfer equation with emission and absorption along a
-    grid of sightlines, at multiple frequencies
+    grid of sightlines, at multiple frequencies. Raytraces in parallel, with number
+    of threads determined by numba set_num_threads
 
     Parameters
     ----------
@@ -45,12 +46,86 @@ def radtransfer(j, m, kappa, x, h, gridres, L, center=0, i0=0):
         power / length^2 / sr / frequency (this is the quantity I_ν in RT)
     """
 
-    #   don't have parallel working yet - trickier than simple surface density map because the order of extinctions and emissions matters
-    # if ncores = -1:
-    #     Nchunks = get_num_threads()
-    # else:
-    #     set_num_threads(ncores)
-    #     Nchunks = ncores
+    Nchunks = get_num_threads()
+    num_bands = j.shape[1]
+
+    # sort by z then split into chunks
+    # print("Sorting...")
+    order = (-x[:, 2]).argsort()
+    # print("Chunking...")
+    j, m, kappa, x, h = (
+        np.array_split(j[order], Nchunks, 0),
+        np.array_split(m[order], Nchunks, 0),
+        np.array_split(kappa[order], Nchunks, 0),
+        np.array_split(x[order], Nchunks, 0),
+        np.array_split(h[order], Nchunks, 0),
+    )
+    # j, m, kappa, x, h = (
+    #     np.split(j,Nchunks,0)
+    #     np.split(m,Nchunks,0),
+    # np.split(kappa,Nchunks,0), np.split(x,Nchunks,0), np.split(h,Nchunks,0)
+
+    intensity = np.empty((Nchunks, gridres, gridres, num_bands))
+    tau = np.empty_like(intensity)
+    # parallel part, raytracing the individual slabs
+    # print("Raytracing...")
+    for c in prange(Nchunks):
+        intensity[c], tau[c] = radtransfer_singlethread(
+            j[c], m[c], kappa[c], x[c], h[c], gridres, L, center, i0
+        )
+
+    tau_total = np.zeros((gridres, gridres, num_bands))
+    I_total = np.zeros((gridres, gridres, num_bands))
+
+    # now we add up total extinction and total integrated intensity from last
+    # chunk to first, attenuating each successive slab by the running extinction
+    # print("summing slabs...")
+    for c in range(Nchunks)[::-1]:
+        I_total += intensity[c] * np.exp(-tau_total)
+        tau_total += tau[c]
+    return I_total
+
+
+@njit(fastmath=True, error_model="numpy")
+def radtransfer_singlethread(j, m, kappa, x, h, gridres, L, center=0, i0=0):
+    """Simple radiative transfer solver
+
+    Solves the radiative transfer equation with emission and absorption along a
+    grid of sightlines, at multiple frequencies
+
+    Parameters
+    ----------
+    j: array_like
+        shape (N, num_freqs) array of particle emissivities per unit mass (e.g.
+        erg/s/g/Hz)
+    m: array_like
+        shape (N,) array of particle masses
+    kappa: array_like
+        shape (N,) array of particle opacities (dimensions: length^2 / mass)
+    x: array_like
+        shape (N,3) array of particle positions
+    h: array_like
+        shape (N,) array containing kernel radii of the particles
+    gridres: int
+        image resolution
+    center: array_like
+        shape (3,) array containing the center coordinates of the image
+    L: float
+        size of the image window in length units
+    i0: array_like, optional
+        shape (num_freqs,) or (gridres,greidres,num_freqs) array of background
+        intensities
+    return_taumap: boolean, optional
+        Whether to return the optical depth map alongside the intensity map
+        (default False)
+
+    Returns
+    -------
+    intensity, tau: array_like, array_like
+        shape (res,res,num_bands) array of integrated intensity, in your units of
+        power / length^2 / sr / frequency (this is the quantity I_ν in RT), and
+        of optical depth in the different bands along the lines of sight
+    """
 
     x -= center
     # get order for sorting by distance from observer - farthest to nearest
@@ -67,7 +142,7 @@ def radtransfer(j, m, kappa, x, h, gridres, L, center=0, i0=0):
 
     intensity = np.zeros((gridres, gridres, num_freqs))
     intensity += i0  # * 4 * np.pi  # factor of 4pi because we divide by that at the end
-
+    taumap = np.zeros_like(intensity)
     dx = L / (gridres - 1)
     N = len(x)
 
@@ -115,11 +190,11 @@ def radtransfer(j, m, kappa, x, h, gridres, L, center=0, i0=0):
                     tau = kappa_i[b] * kernel * mh2
                     fac1 = np.exp(-tau)
                     fac2 = -np.expm1(-tau)
+                    taumap[gx, gy, b] += tau
                     intensity[gx, gy, b] = (
                         fac1 * intensity[gx, gy, b] + fac2 * j_over_4pi_kappa_i[b]
                     )
-
-    return intensity
+    return intensity, taumap
 
 
 HERSCHEL_DEFAULT_WAVELENGTHS = np.array([150, 250, 350, 500])
@@ -196,6 +271,7 @@ def dust_emission_map(
     res,
     wavelengths_um=HERSCHEL_DEFAULT_WAVELENGTHS,
     center_pc=0,
+    parallel=True,
 ) -> np.ndarray:
     """Generates a map of dust emission in cgs units for specified wavelengths,
     neglecting scattering (OK for FIR/submm wavelengths)
