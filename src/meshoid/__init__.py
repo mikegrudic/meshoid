8f6fa214d9eb4cfab8fd773573fc6c6b15fcce5e
Mike Grudic <mike.grudic@gmail.com>
2025-07-28 17:32:54 -0400

    feat: added pregenerated glass file to accelerate glass relaxation

--- src/meshoid/__init__.py
+++ src/meshoid/__init__.py
@@ -3,7 +3,7 @@ from .grid_deposition import *
 from .kernel_density import *
 from .derivatives import *
 from .load_from_snapshot import *
-from .glass import particle_glass
+from .glass import particle_glass, relax_particles
 
 # from .radiation import radtransfer as .radtransfer
 # import radtransfer.radtransfer as GridRadTransfer
--- src/meshoid/glass.py
+++ src/meshoid/glass.py
@@ -3,6 +3,20 @@
 import numpy as np
 from .Meshoid import Meshoid
 from scipy.optimize import minimize_scalar
+import os
+
+
+def tesselate_glass_coords(x):
+    """Tesselates 8 rescaled copies of glass coordinates to make a glass with 8x more particles"""
+    x = np.concatenate(
+        [
+            x / 2 + i * np.array([0.5, 0, 0]) + j * np.array([0, 0.5, 0]) + k * np.array([0, 0, 0.5])
+            for i in range(2)
+            for j in range(2)
+            for k in range(2)
+        ]
+    )
+    return x
 
 
 def particle_glass(N: int = 64**3, L: float = 1.0, dim: int = 3, tol: float = 1e-2, optimize=False) -> np.ndarray:
@@ -24,7 +38,14 @@ def particle_glass(N: int = 64**3, L: float = 1.0, dim: int = 3, tol: float = 1e
     coords: ndarray
         Shape (N,dim) array of coordinate positions of the particle glass arrangement
     """
-    x = np.random.rand(N, dim)
+    x = np.load(os.path.dirname(os.path.abspath(__file__)) + "/glass_64.npy")
+    while len(x) < N:
+        x = tesselate_glass_coords(x)
+    dist = x.max(axis=1)
+    x = x[dist.argsort()][:N]  # sort by manhattan distance and take first N
+    x /= x.max() / (1 - 1e-15)
+    x = x % 1.0
+    #    x = np.random.rand(N, dim)
     relax_particles(x, tol)
     return x * L
 
@@ -47,18 +68,16 @@ def relax_particles(coordinates: np.ndarray, tol: float = 1e-2, optimize=False) 
     if np.any(coordinates > 1):
         raise ValueError("input coordinates to relaxation routine are not inside the unit cube.")
 
-    N = len(coordinates)
     hfrac = 0.5
     i = 0
-    wt = np.repeat(1.0 / N, N)
-    tree_new = Meshoid(coordinates)
+    tree_new = Meshoid(coordinates, boxsize=1.0)
+    wt = 1 / len(coordinates)
     while True:
-        M = tree_new  # Meshoid(coordinates, wt, boxsize=1)
-        rho = M.Density()
+        rho = tree_new.Density()
         rho_std = rho.std()
-        print(hfrac, rho.std(), rho.mean())
+        print(f"iter={i} RMS density variation: {rho_std}")
         dx = (wt / rho) ** (1.0 / 3)
-        gradrho = M.D(rho)
+        gradrho = tree_new.D(rho)
         gradrho_norm = np.sum(gradrho * gradrho, 1) ** 0.5
 
         def new_coords(hfrac):
@@ -73,17 +92,16 @@ def relax_particles(coordinates: np.ndarray, tol: float = 1e-2, optimize=False) 
             hfrac *= 0.9
         else:
             sol = minimize_scalar(new_density_std, [hfrac / 10, hfrac * 10], tol=1e-1)
-            print(sol.x, rho.std())
             hfrac = sol.x
             coords_new = new_coords(hfrac)
             tree_new = Meshoid(coords_new, boxsize=1.0)
 
-        coordinates = coords_new  # (hfrac * dx / gradrho_norm)[:, None] * gradrho
+        coordinates[:] = coords_new  # (hfrac * dx / gradrho_norm)[:, None] * gradrho
 
         if rho.std() < tol * rho.mean():
             break
 
         i += 1
 
-    coordinates -= coordinates[0] - 0.5  # center on 0'th particle
-    coordinates %= 1.0
+    coordinates[:] = coordinates - (coordinates[0] - 0.5)  # center on 0'th particle
+    coordinates[:] = coordinates % 1.0
