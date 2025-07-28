7d4008d0a9f606081f241b645f058152cb1a52de
Mike Grudic <mike.grudic@gmail.com>
2025-07-28 17:37:54 -0400

    version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="meshoid",
-    version="1.48.0",
+    version="1.48.1",
     description="Package for analysis of meshless simulation data",
     #      long_description=long_description,
     url="http://github.com/mikegrudic/meshoid",
