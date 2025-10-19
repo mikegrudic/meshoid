75af125f49c4f6c746a74969a33e10dc5887ffad
Mike Grudic <mgrudic@ccalin030.flatironinstitute.org>
2025-10-19 17:09:09 -0400

    fix: correct method in Meshoid.Projection

--- src/meshoid/Meshoid.py
+++ src/meshoid/Meshoid.py
@@ -700,7 +700,7 @@ class Meshoid:
             center = self.center
         if size is None:
             size = self.L
-        return SurfaceDensity(
+        return GridSurfaceDensity(
             f * self.vol,
             self.pos,
             np.clip(smooth_fac * self.kernel_radius, 2 * size / res, 1e100),
